b2166cf15a3c633a2a0afc869834f05af3e995f1
diponroy <diponsust@outlook.com>
2020-11-06 21:20:28 +0600

    error manage

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-from fastapi import Depends, FastAPI, Header, HTTPException
+from fastapi import Depends, FastAPI, Header, HTTPException, Request
+from fastapi.responses import JSONResponse
 
-from app.api import item_api, user_api, employee_api
+
+from app.api import item_api, user_api, employee_api, error_api
+from app.errors import UnicornException
+
 
 app = FastAPI()
+
+
+
 
 
 async def get_token_header(x_token: str = Header(...)):
@@ -10,6 +17,25 @@ async def get_token_header(x_token: str = Header(...)):
         raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
+# https://fastapi.tiangolo.com/tutorial/handling-errors/
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    # print(f"OMG! An HTTP error!: {repr(exc)}")
+    return JSONResponse(
+        status_code=500,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, exc: Exception):
+    # print(f"OMG! An HTTP error!: {repr(exc)}")
+    return JSONResponse(
+        status_code=500,
+        content={"message": "Internal Server Error"},
+    )
+
+
+app.include_router(error_api.router)
 app.include_router(user_api.router)
 app.include_router(
     item_api.router,
--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from app.errors import UnicornException
 
 app = FastAPI()
+
 
 
 @app.get("/hello")
 def get_list():
     return {"Hello": "World"}
-
 
 @app.get("/hello/{item_id}")
 def get_item(item_id: int, q: Optional[str] = None):
@@ -27,3 +30,32 @@ def post_item(item_id: int, q: Optional[str] = None):
 @app.delete("/hello/{item_id}")
 def delete_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+
+
+
+# ----------------------------------------- Error --------------------------------------------------
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    # print(f"OMG! An HTTP error!: {repr(exc)}")
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, exc: Exception):
+    # print(f"OMG! An HTTP error!: {repr(exc)}")
+    return JSONResponse(
+        status_code=500,
+        content={"message": "Internal Server Error"},
+    )
+
+@app.get("/error")
+def error_list():
+    raise UnicornException(name="Not managed error.")
+
+@app.get("/error/{item_id}")
+def error():
+    raise Exception("Not managed error.")
