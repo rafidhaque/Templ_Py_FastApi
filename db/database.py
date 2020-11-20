9f03ca8264ded118a99ef8c2000fff2a2a8fd2c8
diponroy <diponsust@outlook.com>
2020-11-20 10:01:22 +0600

    docker db

--- db/database.py
+++ db/database.py
@@ -3,17 +3,22 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./test_sql_app.db"
-SQLALCHEMY_DATABASE_URL = "sqlite:///./__temp_app__/test_sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./__temp_app__/test_sql_app.db"
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://admin:secret@192.168.1.224:54320/postgres?sslmode=prefer"
 # DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port,
 #                                                                database_name, db_ssl_mode)
 
-
+# SQLALCHEMY_DATABASE_URL = "postgresql://admin:secret@192.168.1.224:54320/postgres?sslmode=prefer"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:secret@127.0.0.1:5001/postgres?sslmode=prefer"
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, pool_size=3, max_overflow=0
 )
+
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
