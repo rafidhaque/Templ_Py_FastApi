467d0fa1185244c42c8e9d3a1f597771cc5a0ff3
rafidhaque <rafidhaque@iut-dhaka.edu>
2021-02-08 17:37:06 +0600

    Test cases added

---
+++ testing/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class AccountApikeyPurpose(Enum):
+    WEB_FROM = "Web Form"
+    WEB_CHAT = "Web Chat"
---
+++ testing/test_Enums.py
@@ -0,0 +1,11 @@
+import unittest
+from testing import enums
+
+
+class TestEnum(unittest.TestCase):
+    def test_enums_web_chat(self):
+        self.assertEqual(enums.AccountApikeyPurpose.WEB_CHAT.value, "Web Chat")
+        self.assertNotEqual(enums.AccountApikeyPurpose.WEB_CHAT.value, "web chat")
+
+    def test_enums_web_form(self):
+        self.assertEqual(enums.AccountApikeyPurpose.WEB_FROM.value, "Web Form")
